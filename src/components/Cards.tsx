c0fb618df4dd5773e5ed6107756093d48bee962c
Nathalia Galante <nathalia.g.a@hotmail.com>
2022-01-06 16:24:33 -0300

    feat: card component

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,8 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { Table } from 'antd';
+import { Button, Space, Table } from 'antd';
+import { Container } from "react-bootstrap";
 
 const Cards = (props: { url: any; }) => {
     const [cards, setCards] = useState([]);
+    // const [disabled, setDisabled] = useState(false);
+
+    // const getCards = () => {
+    //     fetch(`${props.url}/cards`)
+    //       .then(response => response.json())
+    //       .then(data => setCards(data));
+    // }
+
+    const changeCardStatus = (id: any, type: any) => {
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ status: type === 'a' ? 'Approved' : 'Rejected' })
+        };
+        fetch(`${props.url}/cards/${id}`, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                // getCards();
+                // setDisabled(true);
+            })
+            .catch(error => console.log(error));
+        
+    }
 
     const columns = [
         {
@@ -32,6 +57,20 @@ const Cards = (props: { url: any; }) => {
             key: 'updatedAt',
             render: (item: null) => item === null ? '-' : item
         },
+        {
+            title: 'Actions',
+            key: '6',
+            render: (record: any) => (
+                <Space size="middle">
+                    <Button  type="primary" onClick={() => changeCardStatus(record.id, 'a')}>
+                    Approve
+                    </Button>
+                    <Button  danger onClick={() => changeCardStatus(record.id, 'r')}>
+                    Reject
+                    </Button>
+                </Space>
+            ),
+        }
     ]
 
     useEffect(() => {
@@ -40,22 +79,21 @@ const Cards = (props: { url: any; }) => {
             return response.json();
           })
           .then(setCards);
-      }, [props.url]);
+    }, [props.url]);
 
 
     return (
-        <>
-        <div>
+        <Container>
             <h1>Cards</h1>
             <Table 
                 columns={columns} 
                 dataSource={cards} 
                 pagination={{ pageSize: 5 }}
                 size="middle"
+                rowKey="id"
             > 
             </Table>
-        </div>
-        </>
+        </Container>
     );
 }
 
